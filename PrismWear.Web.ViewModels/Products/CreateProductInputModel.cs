cd6af36770974170a961661cb11e4d9885c21198
RadoPenev <radomir.penev16@gmail.com>
2025-01-22 15:02:14 +0200

    Added a project for the view models

---
+++ PrismWear.Web.ViewModels/Products/CreateProductInputModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrismWear.Web.ViewModels
+{
+    public class CreateProductInputModel
+    {
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [StringLength(100, ErrorMessage = "The name must be between {2} and {1} characters.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(500, ErrorMessage = "The description must not exceed {1} characters.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be a positive value.")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "Size is required.")]
+        [StringLength(10, ErrorMessage = "The size must not exceed {1} characters.")]
+        public string Size { get; set; }
+
+        [Required(ErrorMessage = "Image path is required.")]
+        public IEnumerable<IFormFile> Images { get; set; }
+
+        [Required(ErrorMessage = "Category is required.")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<KeyValuePair<string, string>> CategoriesItems { get; set; }
+
+    }
+}
